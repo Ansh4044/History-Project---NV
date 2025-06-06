56c0881e6c189e959d76c6be06fc8cec3d5a01f4
Ansh Kumar <154911321+Ansh4044@users.noreply.github.com>
2025-06-07 00:18:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function line1() {
+   document.getElementById("line").style.width = "75px";
+   document.getElementById("line").style.right = "119px";
+   document.getElementsByClassName("summary")[0].style.color = "#00ffff";
+   document.getElementsByClassName("mcq")[0].style.color = "#fdffe3";
+   document.getElementsByClassName("about")[0].style.color = "#fdffe3";
+   document.getElementsByClassName("main-content")[0].innerHTML = '<div class="main-content-1"> <h2>1. The French Revolution and the Idea of the Nation</h2> <center><img src="1.jpg" alt="Failed to load !!!" class="main-content-1-img"></center> <ul> <li>The French Revolution (1789) introduced the idea of nation-states.</li> <li>People became citizens of a nation, not subjects of a king.</li> <li>Values like liberty, equality, fraternity spread.</li> <li>New symbols like flags, anthems, and national holidays were introduced.</li> <li>French armies spread these ideas to other parts of Europe.</li> <li>Napoleon brought reforms but also controlled people.</li> </ul> <h2>2. The Making of Nationalism in Europe</h2> <center><img src="2.jpg" alt="Failed to load !!!" class="main-content-1-img"></center> <ul> <li>Europe had many small kingdoms, not unified nations.</li> <li>Nationalism grew through shared language, culture, and history.</li> <li>Middle class wanted unity, equal laws, and freedom.</li> <li>Conservatives supported monarchy but later accepted slow change.</li> <li>Secret societies worked to bring revolution and unity.</li> </ul> <h2>3. The Age of Revolutions: 1830–1848</h2> <center><img src="3.jpg" alt="Failed to load !!!" class="main-content-1-img"></center> <ul> <li>Many revolutions happened demanding freedom and rights.</li> <li>Greek War of Independence began in 1830 against Ottoman rule.</li> <li>Romanticism in art and literature spread nationalism.</li> <li>Poets and artists glorified the idea of nations.</li> <li>In 1848, revolts spread across Europe, mostly failed due to lack of unity.</li> </ul> <h2>4. The Making of Germany and Italy</h2> <h3>Germany:</h3> <center><img src="4.1.1.jpg" alt="Failed to load !!!" class="main-content-1-img"></center> <ul> <li>Germany had many small states earlier.</li> <li>Otto von Bismarck united Germany using wars and diplomacy.</li> <li>Germany was united in 1871 under the Prussian king.</li> </ul> <h3>Italy:</h3> <center><img src="4.1.jpg" alt="Failed to load !!!" class="main-content-1-img"></center> <center><img src="4.2.jpg" alt="Failed to load !!!" class="main-content-1-img"></center> <ul> <li>Italy was also divided into many states.</li> <li>Giuseppe Mazzini and Garibaldi played important roles.</li> <li>Garibaldi army (Red Shirts) helped unite southern Italy.</li> <li>In 1861, Victor Emmanuel II became king of united Italy.</li> </ul> <h2>5. Visualising the Nation</h2> <center><img src="5.jpg" alt="Failed to load !!!" class="main-content-1-img"></center> <ul> <li>People imagined the nation as a person (female figure).</li> <li>Example: Germania for Germany, Bharat Mata in India.</li> <li>Images used in paintings, coins, and statues to create unity.</li> <li>Symbols like swords, crowns, and laurel leaves showed strength and victory.</li> </ul> <h2>6. Nationalism and Imperialism</h2> <center><img src="6.jpg" alt="Failed to load !!!" class="main-content-1-img"></center> <ul> <li>Nationalism turned aggressive after 1871.</li> <li>European countries competed for power and land.</li> <li>Balkan region became a tense area with ethnic groups wanting freedom.</li> <li>This rivalry led to the beginning of World War I.</li> </ul> </div>';
+};
+
+function line2() {
+   document.getElementById("line").style.width = "40px";
+   document.getElementById("line").style.right = "-15px";
+   document.getElementsByClassName("mcq")[0].style.color = "#00ffff";
+   document.getElementsByClassName("about")[0].style.color = "#fdffe3";
+   document.getElementsByClassName("summary")[0].style.color = "#fdffe3";
+   document.getElementsByClassName("main-content")[0].innerHTML = '<div class="main-content-2"> <h1>Select your topic <br> for attempting <br>MCQ</h1> <a href="mcq1.html"> <div class="mcq-1"> <center> <img src="1.jpg" alt="Failed to load !!!"> <h3>The French Revolution and <br> the Idea of the Nation</h3> </center> </div> </a> <a href="mcq2.html"> <div class="mcq-1"> <center> <img src="2.jpg" alt="Failed to load !!!"> <h3>The Making of <br>Nationalism in Europe</h3> </center> </div> </a> <a href="mcq3.html"> <div class="mcq-1"> <center> <img src="3.jpg" alt="Failed to load !!!"> <h3>The Age of Revolutions:<br> 1830–1848</h3> </center> </div> </a> <a href="mcq4.html"> <div class="mcq-1"> <center> <img src="4.1.1.jpg" alt="Failed to load !!!"> <h3>The Making of <br>Germany and Italy</h3> </center> </div> </a> <a href="mcq5.html"> <div class="mcq-1"> <center> <img src="5.jpg" alt="Failed to load !!!"> <h3>Visualising the Nation</h3> </center> </div> </a> <a href="mcq6.html"> <div class="mcq-1"> <center> <img src="6.jpg" alt="Failed to load !!!"> <h3>Nationalism and Imperialism</h3> </center> </div> </a> </div>';
+};
+
+function line3() {
+   document.getElementById("line").style.width = "50px";
+   document.getElementById("line").style.right = "-132px";
+   document.getElementsByClassName("about")[0].style.color = "#00ffff";
+   document.getElementsByClassName("mcq")[0].style.color = "#fdffe3";
+   document.getElementsByClassName("summary")[0].style.color = "#fdffe3";
+   document.getElementsByClassName("main-content")[0].innerHTML = '<div class="main-content-3"> <div class="creator"> <i> <h2 style="color: #00ffff; margin: 0px;">About the Creator :-</h2> </i> <p> Hi! I am <span>Anushka Bharti</span>, the creator of this website. I am a student of <span>Naman Vidya</span>, currently studying in <span>Class 10</span>. This website is my summer vacation project, and it contains a brief summary and multiple-choice questions (MCQs) for every sub-topic of History Chapter 1, according to the NCERT syllabus – <span>"The Rise of Nationalism in Europe."</span> </p> <i> <h2 style="color: #00ffff;">About the Project :-</h2> </i> <p>This project is in the form of a website. It includes a brief summary and MCQs for each sub-topic of History Chapter 1 – "The Rise of Nationalism in Europe" – based on the NCERT syllabus. It is designed to help students like me to revise and practice the chapter in an easy and interactive way.</p> <i> <h2 style="color: #00ffff;">About the Assigner :-</h2> </i> <p>This project was assigned by my History teacher, He is a very dedicated and inspiring teacher who focuses on the overall development and creativity of his students. </p> <h2 style="color: #00ffff;">Me in my school :-</h2> </i> <p> <span style="font-weight: bold;">Name :-</span> Anushka Bharti<br> <span style="font-weight: bold;">Class :-</span> 10<br>  <span style="font-weight: bold;">Roll No. :-</span> 03 <br> </p> <div class="footer"> Created with ❤️ by Anushka </div> </div> </div>';
+};
+
+
+window.addEventListener("pageshow", function (event) {
+   if (event.persisted || performance.getEntriesByType("navigation")[0].type === "back_forward") {
+      location.reload();
+   }
+});
+
+window.addEventListener("pageshow", function (event) {
+    if (event.persisted) {
+      location.reload();
+    }
+  });
